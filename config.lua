779cd44db749366717e1394033c00a09c008bfe4
q107580018 <107580018@qq.com>
2022-04-30 21:44:23 +0800

    update at 20220430

---
+++ config.lua
@@ -0,0 +1,233 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<space>t"] = ":set splitright<cr>:vsplit<cr>:term<cr>i"
+lvim.keys.normal_mode["<S-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<space>q"] = ":Q<cr>"
+lvim.keys.normal_mode["<space>wq"] = ":wq<cr>"
+lvim.keys.normal_mode["<space>v"] = ":sp<cr>"
+lvim.keys.normal_mode["<space>s"] = ":vs<cr>"
+lvim.keys.normal_mode["<C-l>"] = "<C-w>l"
+lvim.keys.normal_mode["<C-h>"] = "<C-w>h"
+lvim.keys.normal_mode["<C-j>"] = "<C-w>j"
+lvim.keys.normal_mode["<C-k>"] = "<C-w>k"
+function CompileRun()
+    local filetype = vim.bo.filetype
+    if filetype == "go" then
+        vim.cmd("!go run %")
+    elseif filetype == "python" then
+        vim.cmd("!python3 %")
+    end
+
+end
+lvim.keys.normal_mode["zr"] = ":lua CompileRun()<cr>"
+-----plug keymappings
+lvim.builtin.which_key.mappings = {}
+lvim.keys.normal_mode["<F3>"] = ":NvimTreeToggle<cr>"
+lvim.keys.normal_mode["<space>ff"] = ":Telescope find_files<cr>"
+lvim.keys.normal_mode["<space>fg"] = ":Telescope live_grep<cr>"
+lvim.keys.normal_mode["K"] = ":lua vim.lsp.buf.hover()<cr>"
+lvim.keys.normal_mode["<leader>rn"]= ":lua vim.lsp.buf.rename()<cr>"
+lvim.keys.normal_mode["<leader>ca"]= ":lua vim.lsp.buf.code_action()<cr>"
+lvim.keys.normal_mode["gd"] = ":lua vim.lsp.buf.definition()<cr>"
+lvim.keys.normal_mode["gD"] = ":lua vim.lsp.buf.declaration()<cr>"
+lvim.keys.normal_mode["[g"] = ":lua vim.diagnostic.goto_prev()<cr>"
+lvim.keys.normal_mode["]g"] = ":lua vim.diagnostic.goto_next()<cr>"
+
+lvim.keys.normal_mode["<leader>1"] = ":BufferLineGoToBuffer 1<cr>"
+lvim.keys.normal_mode["<leader>2"] = ":BufferLineGoToBuffer 2<cr>"
+lvim.keys.normal_mode["<leader>3"] = ":BufferLineGoToBuffer 3<cr>"
+lvim.keys.normal_mode["<leader>4"] = ":BufferLineGoToBuffer 4<cr>"
+lvim.keys.normal_mode["<leader>5"] = ":BufferLineGoToBuffer 5<cr>"
+lvim.keys.normal_mode["<leader>6"] = ":BufferLineGoToBuffer 6<cr>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = false
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+local _, actions = pcall(require, "bufferline.actions")
+lvim.builtin.bufferline.options.separator_style = "slant"
+
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+-- }
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  -- "c",
+  -- "javascript",
+  "json",
+  "lua",
+  "python",
+  -- "typescript",
+  -- "tsx",
+  "css",
+  -- "rust",
+  -- "java",
+  -- "yaml",
+  "go",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- generic LSP settings
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
+--   {
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
+
+-- Additional Plugins
+-- lvim.plugins = {
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
+-- WSL跟系统共享剪贴板
+if vim.fn.has('wsl') then
+    vim.cmd [[
+  augroup Yank
+  autocmd!
+  autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
+  augroup END
+  ]]
+end
+-- Nvimtree auto close
+vim.api.nvim_command("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
